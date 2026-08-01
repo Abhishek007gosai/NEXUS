15b5f9de3905afb0c428866f7d096067c85a913f
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-08-02 01:58:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -82,8 +82,7 @@ MESSAGES = {
     "FSUB_PHOTO": "https://i.ibb.co/sdYHCnBC/tmp9peum4mg.jpg",
     "SHORT_PIC": "https://i.ibb.co/sdYHCnBC/tmp9peum4mg.jpg",
     "SHORT": "https://i.ibb.co/sdYHCnBC/tmp9peum4mg.jpg",
-    # Custom image shown on every bot anime search result (paste https URL)
-    "SEARCH_PHOTO": "",
+    "SEARCH_PHOTO": "https://i.ibb.co/sdYHCnBC/tmp9peum4mg.jpg",
 }
 
 # Aliases for /anidex
