6f2a93eb9d920ba3f3c11c7766f63ba18aa8fc87
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-08-05 10:34:13 +0530

    Delete plugins directory

--- plugins/sources.py
+++
@@ -1,6 +0,0 @@
-"""Registry of available metadata sources (AniList for adult anime / manga)."""
-from plugins.anilist import AniListSource
-
-anilist = AniListSource()
-SOURCES = {"anilist": anilist}
-__all__ = ["SOURCES", "anilist"]
